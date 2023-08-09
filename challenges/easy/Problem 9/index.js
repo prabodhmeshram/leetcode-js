1749c8a945cab1a8a0374688b283803e9093ddb9
Prabodh Meshram <prabodh.meshram7@gmail.com>
2023-08-09 20:48:09 +0530

    Further refactorings and adding readme

--- challenges/easy/Problem 9/index.js
+++ challenges/easy/Problem 9/index.js
@@ -4,20 +4,12 @@
  * @return {boolean}
  */
 var isPalindrome = function (x) {
-  // if a negative number, no need to
-  // check, its already not a palindrome
   if (x < 0) return false;
-
   let result = 0;
   let number = x;
-  let rightmost;
 
   while (number > 0) {
-    // Getting the rightmost digit
-    rightmost = number % 10;
-
-    result = result * 10 + rightmost;
-
+    result = result * 10 + (number % 10);
     // Removing the rightmost digit from the number
     number = Math.floor(number / 10);
   }
---
+++ challenges/easy/Problem 9/readme.md
@@ -0,0 +1,16 @@
+## Multiple Approaches to solve the problem
+
+### First approach
+
+In first approach went ahead with the inbuilt methods of JS
+and the performance was not not upto the mark, although the solution was accepted
+
+### Second approach
+
+In second approach went with the traditional way to reverse a positive number,
+later optimized the code a bit more to reduce the memory consumption
+by getting rid of some variable
+
+### Stats from LeetCode
+
+![Alt text](image.png)
