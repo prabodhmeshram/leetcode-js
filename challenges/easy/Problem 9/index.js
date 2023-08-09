71bceb5275762cc4ec27d92d843af6735cfe3b47
Prabodh Meshram <prabodh.meshram7@gmail.com>
2023-08-09 20:41:42 +0530

    Optimized the code and went ahead without inbuilt methods

--- challenges/easy/Problem 9/index.js
+++ challenges/easy/Problem 9/index.js
@@ -1,8 +1,25 @@
 /**
+ * https://leetcode.com/problems/palindrome-number
  * @param {number} x
  * @return {boolean}
  */
 var isPalindrome = function (x) {
-  let strNum = "" + x + "";
-  return strNum === strNum.split("").reverse().join("");
+  // if a negative number, no need to
+  // check, its already not a palindrome
+  if (x < 0) return false;
+
+  let result = 0;
+  let number = x;
+  let rightmost;
+
+  while (number > 0) {
+    // Getting the rightmost digit
+    rightmost = number % 10;
+
+    result = result * 10 + rightmost;
+
+    // Removing the rightmost digit from the number
+    number = Math.floor(number / 10);
+  }
+  return result === x;
 };
